0a1f771a4c003cc71fb08f1c9aea72f10fcb3f6f
Kariuki11 <irungukenneth433@gmail.com>
2024-06-13 04:10:23 +0300

    New commits

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    return render(request, 'post/post_list.html')
+# posts/views.py
+
+
+
+def post_list(request):
+    return HttpResponse("This is the list of posts.")
